cc50dbf718b856768fa239d456784285af5bc061
vassuraw <vishrawat0509@hotmail.com>
2021-05-11 07:45:19 +0530

    ConFusion Server

---
+++ routes/dishRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter
+  .route("/")
+  .all((request, response, next) => {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((request, response, next) => {
+    response.end("Will send all the dishes to you!");
+  })
+  .post((request, response, next) => {
+    response.end(
+      `Will add the dish: ${request.body.name}  with details ${request.body.description}`
+    );
+  })
+  .put((request, response, next) => {
+    response.statusCode = 403;
+    response.end("PUT operation is not supported on /dishes");
+  })
+  .delete((request, response, next) => {
+    response.end("Deleting all the dishes");
+  });
+
+dishRouter
+  .route("/:dishId")
+  .get((request, response, next) => {
+    response.end(
+      `Will send details of the dish: ${request.params.dishId} to you!`
+    );
+  })
+  .post((request, response, next) => {
+    response.statusCode = 403;
+    response.end(
+      `POST operation not supported on /dishes/${request.params.dishId}`
+    );
+  })
+  .put((request, response, next) => {
+    response.write(`Updating the dish: ${request.params.dishId}\n`);
+    response.end(
+      `Will update the dish: ${request.body.name} with details: ${request.body.description}`
+    );
+  })
+  .delete((request, response, next) => {
+    response.end(`Deleting dish: ${request.params.dishId}`);
+  });
+
+module.exports = dishRouter;
---
+++ routes/leaderRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter
+  .route("/")
+  .all((request, response, next) => {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((request, response, next) => {
+    response.end("Will send all the leaders to you!");
+  })
+  .post((request, response, next) => {
+    response.end(
+      `Will add the leader: ${request.body.name}  with details ${request.body.description}`
+    );
+  })
+  .put((request, response, next) => {
+    response.statusCode = 403;
+    response.end("PUT operation is not supported on /leaders");
+  })
+  .delete((request, response, next) => {
+    response.end("Deleting all the leaders");
+  });
+
+leaderRouter
+  .route("/:leaderId")
+  .get((request, response, next) => {
+    response.end(
+      `Will send details of the leader: ${request.params.leaderId} to you!`
+    );
+  })
+  .post((request, response, next) => {
+    response.statusCode = 403;
+    response.end(
+      `POST operation not supported on /leaders/${request.params.leaderId}`
+    );
+  })
+  .put((request, response, next) => {
+    response.write(`Updating the leader: ${request.params.leaderId}\n`);
+    response.end(
+      `Will update the leader: ${request.body.name} with details: ${request.body.description}`
+    );
+  })
+  .delete((request, response, next) => {
+    response.end(`Deleting leader: ${request.params.leaderId}`);
+  });
+
+module.exports = leaderRouter;
