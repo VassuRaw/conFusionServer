547d1c6edf80deb6d0b999216af7d7a0b4ef6eac
vassuraw <vishrawat0509@hotmail.com>
2021-05-13 15:49:25 +0530

    Express Session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -31,35 +33,28 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+// app.use(cookieParser("12345-67890-09876-54321"));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 function auth(request, response, next) {
-  console.log(request.signedCookies);
-  if (!request.signedCookies.user) {
-    var authHeader = request.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authenticated");
-      response.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == "admin" && pass == "password") {
-      response.cookie("user", "admin", { signed: true });
-      next(); //Authorized
-    } else {
-      var err = new Error("You are not authorized");
-      response.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
+  console.log(request.session);
+  if (!request.session.user) {
+    var err = new Error("You are not authenticated");
+    err.status = 403;
+    return next(err);
   } else {
-    if (request.signedCookies.user === "admin") {
+    if (request.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("You are not authenticated");
@@ -73,8 +68,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
