9621f05084d5c038f10c84e6d012ea5201f34b71
vassuraw <vishrawat0509@hotmail.com>
2021-05-13 16:34:41 +0530

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -44,23 +46,27 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(request, response, next) {
   console.log(request.session);
-  if (!request.session.user) {
+  if (!request.user) {
     var err = new Error("You are not authenticated");
     err.status = 403;
     return next(err);
   } else {
-    if (request.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated");
-      err.status = 401;
-      next(err);
-    }
+    next();
+    // if (request.session.user === "authenticated") {
+    //   next();
+    // } else {
+    //   var err = new Error("You are not authenticated");
+    //   err.status = 401;
+    //   next(err);
+    // }
   }
 }
 
